079aaf2dfc621fbfd47fbbf1e8caa8776bc1c12b
Valentin Rupprecht <valentin.rupprecht@yahoo.de>
2020-01-29 00:45:37 +0100

    added class info

--- Ex3/get_input_vectors.py
+++ Ex3/get_input_vectors.py
@@ -2,35 +2,45 @@ def create_input_vectors():
 	out = ""
 	counter = 0
 	skip_first = True
-	with open("data/mnist_784_sampled.csv", "r") as f_in:
-		with open("data/input_vector_file_sampled.in", "a") as f_out:
-			f_out.write("$TYPE inputvec\n" + \
-						"$XDIM 10000\n" + \
-						"$YDIM 1\n" + \
-						"$VEC_DIM 784\n\n")
-			for line in f_in:
-				if skip_first:
-					skip_first = False
-					continue
+	with open("data/mnist_784_sampled.csv", "r") as f_csv:
+		with open("data/input_vector_file_sampled.in", "a") as f_vec:
+			with open("data/classes_samples.cls", "a") as f_cls:
+				f_cls.write("$NUM_CLASSES 10\n" +
+							"$CLASS_NAMES 0 1 2 3 4 5 6 7 8 9\n" +
+							"$XDIM 2\n" +
+							"$YDIM {}\n\n".format(num_vectors))
 
-				splited = line.split(",")[:-1]
-				f_out.write(" ".join(splited) + " " + str(counter) + "\n")
-				counter += 1
+				f_vec.write("$TYPE inputvec\n" + \
+							"$XDIM {}\n".format(num_vectors) + \
+							"$YDIM 1\n" + \
+							"$VEC_DIM 784\n\n")
+				for line in f_csv:
+					if skip_first:
+						skip_first = False
+						continue
 
-				if counter % 1000 == 0:
-					print(counter)
+					splited = line.split(",")
+					label = int(splited[-1])
+					splited = splited[:-1]
+					f_vec.write(" ".join(splited) + " " + str(counter) + "\n")
+					f_cls.write(str(counter) + " " + str(label) + "\n")
+					counter += 1
+
+					if counter % 1000 == 0:
+						print(counter)
 
 
 def create_template_vectors():
 	with open("data/template_vector_file_sampled.tv", "a") as f:
 		f.write("$TYPE template\n" +
 				"$XDIM 7\n" +
-				"$YDIM 10000\n" +
+				"$YDIM {}\n".format(num_vectors) +
 				"$VECDIM 784\n\n")
 		for i in range(784):
 			f.write("{} pixel{} 1 1 1 1 1\n".format(i, i + 1))
 
 
 if __name__ == '__main__':
+	num_vectors = 10000
 	create_input_vectors()
 	create_template_vectors()
