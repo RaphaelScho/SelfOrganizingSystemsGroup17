2e40feff3e7699c7e8259e6c8aa58ae4aea0e33f
Rapheal Schotola <e1225193@student.tuwien.ac.at>
2020-01-29 18:57:32 +0100

    outpath can be specified for viz

--- Ex3/run.sh
+++ Ex3/run.sh
@@ -3,5 +3,5 @@ then
 	eval $"somtoolbox/somtoolbox.sh GrowingSOM som.prop"
 elif [ $1 == "viz" ] 
 then
-	eval $"somtoolbox/somtoolbox.sh SOMViewer -u data/out/group17.unit.gz -w data/out/group17.wgt.gz --dw data/out/group17.dwm.gz -c data/classes_sampled1000.cls -v data/input_vector_file_sampled1000.in -t data/template_vector_file_sampled1000.tv --colours data/out/colors.txt --vis MetroMap"
+	eval $"somtoolbox/somtoolbox.sh SOMViewer -u data/$2/group17.unit.gz -w data/$2/group17.wgt.gz --dw data/$2/group17.dwm.gz -c data/classes_sampled1000.cls -v data/input_vector_file_sampled1000.in -t data/template_vector_file_sampled1000.tv --colours data/out/colors.txt --vis MetroMap"
 fi
