7088063b28a283df112c6ee8e50a44495141bbec
Valentin Rupprecht <valentin.rupprecht@yahoo.de>
2020-01-29 16:58:07 +0100

    edit run.sh

---
+++ Ex3/run.sh
@@ -0,0 +1,7 @@
+if [ $1 == "train" ]
+then 
+	eval $"somtoolbox/somtoolbox.sh GrowingSOM som.prop"
+elif [ $1 == "viz" ] 
+then
+	eval $"./somtoolbox.sh SOMViewer -u data/out/group17.unit.gz -w data/out/group17.wgt.gz --dw data/out/group17.dwm.gz -c data/classes_sampled1000.cls -v data/input_vector_file_sampled1000.in"
+fi
