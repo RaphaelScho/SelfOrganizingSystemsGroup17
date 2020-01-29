b586695234c5f45f100b404398584c2a23f9dc7e
Valentin Rupprecht <valentin.rupprecht@yahoo.de>
2020-01-29 17:00:12 +0100

    edit run.sh

--- Ex3/run.sh
+++ Ex3/run.sh
@@ -3,5 +3,5 @@ then
 	eval $"somtoolbox/somtoolbox.sh GrowingSOM som.prop"
 elif [ $1 == "viz" ] 
 then
-	eval $"./somtoolbox.sh SOMViewer -u data/out/group17.unit.gz -w data/out/group17.wgt.gz --dw data/out/group17.dwm.gz -c data/classes_sampled1000.cls -v data/input_vector_file_sampled1000.in"
+	eval $"somtoolbox/somtoolbox.sh SOMViewer -u data/out/group17.unit.gz -w data/out/group17.wgt.gz --dw data/out/group17.dwm.gz -c data/classes_sampled1000.cls -v data/input_vector_file_sampled1000.in"
 fi
